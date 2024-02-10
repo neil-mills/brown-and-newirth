24e2f608bc15d12f29fb36566e305e5955b52106
Neil Mills <mail@neil-mills.co.uk>
2024-02-10 13:58:18 +0000

    move front-end fetch request into backend api to fix CORS exception

--- data/fetchData.ts
+++ data/fetchData.ts
@@ -2,9 +2,7 @@ import axios from 'axios'
 import { Product } from '@/app/types'
 
 const fetchData = async () => {
-  const res = await axios.get<Product[]>(
-    'https://www.brownandnewirth.com/wp-json/productData/v1/data'
-  )
+  const res = await axios.get<Product[]>('/api/products')
   return res.data
 }
 
