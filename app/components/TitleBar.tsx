7510aef628a5df977f182e7c3eb2afaf42373877
Neil Mills <mail@neil-mills.co.uk>
2024-03-08 09:55:27 +0000

    Update to title bar component

--- app/components/TitleBar.tsx
+++ app/components/TitleBar.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from 'react'
 
 const TitleBar = ({ children }: PropsWithChildren) => {
   return (
-    <div className="title-br-top bg-grey text-center text-uppercase letter-spacing text-xs">
+    <div className="title-br-top bg-grey text-center text-uppercase letter-spacing text-xs mb-16px">
       {children}
     </div>
   )
