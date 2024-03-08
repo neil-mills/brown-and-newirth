fa61fa34c93d914889c1255ae57f97655fae3e8a
Neil Mills <mail@neil-mills.co.uk>
2024-03-08 09:55:07 +0000

    Updates to category card component

--- app/components/CategoryCard.tsx
+++ app/components/CategoryCard.tsx
@@ -4,26 +4,25 @@ import { Product, Variation } from '../types'
 import { StyleMap } from '../maps'
 
 interface Props {
-  item: Product | Variation | StyleMap
-  type: 'product' | 'style'
+  item: StyleMap
 }
-const isProduct = (item: Product | Variation | StyleMap): item is Product => {
-  return (item as Product).productId !== undefined
-}
+// const isProduct = (item: Product | Variation | StyleMap): item is Product => {
+//   return (item as Product).productId !== undefined
+// }
 
-const isVariation = (item: Product | Variation): item is Variation => {
-  return (item as Variation)['variation-id'] !== undefined
-}
+// const isVariation = (item: Product | Variation): item is Variation => {
+//   return (item as Variation)['variation-id'] !== undefined
+// }
 
-const isStyle = (item: Product | Variation | StyleMap): item is StyleMap => {
-  return (item as StyleMap).label !== undefined
-}
+// const isStyle = (item: Product | Variation | StyleMap): item is StyleMap => {
+//   return (item as StyleMap).label !== undefined
+// }
 
-export const CategoryCard = ({ item, type }: Props) => {
+export const CategoryCard = ({ item }: Props) => {
   return (
     <div className="col-6 col-sm-4 col-lg-6 col-xl-4 col-xxl-3 col-product-grid">
       <Link
-        href="#"
+        href={`/category/${item.slug}`}
         className="product-grid-item style-1 letter-spacing bg-cover d-flex flex-column justify-content-between position-relative"
       >
         {/* <Image
@@ -34,10 +33,7 @@ export const CategoryCard = ({ item, type }: Props) => {
           sizes="(max-width: 480px) auto, (max-width: 768px) auto, auto"
         /> */}
         <img className="img-fluid w-100" src={item.image} alt={item.label} />
-        <div>
-          {isProduct(item) && item.name}
-          {isStyle(item) && item.label}
-        </div>
+        <div>{item.label}</div>
       </Link>
     </div>
   )
