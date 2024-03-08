4d9e81fab13d507e88034a401f3e39954b6670d2
Neil Mills <mail@neil-mills.co.uk>
2024-03-08 14:55:57 +0000

    Remove redundant hook for alt options

--- app/components/AltOptions.tsx
+++ app/components/AltOptions.tsx
@@ -1,13 +1,3 @@
-import React from 'react'
-import { ProductGrid } from '@/app/components'
-import { useAltVariations } from '@/app/hooks'
-
 export const AltOptions = () => {
-  const products = useAltVariations()
-  return (
-    <section>
-      <h3>Alternatives ({products.length})</h3>
-      <ProductGrid type="product" items={products} />
-    </section>
-  )
+  return <section>Alt</section>
 }
