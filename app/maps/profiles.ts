86eedd246b4d4fb767caa19538d944b4da13e3bb
Neil Mills <mail@neil-mills.co.uk>
2024-03-08 14:36:59 +0000

    Add profiles mapping

---
+++ app/maps/profiles.ts
@@ -0,0 +1,34 @@
+import { Map } from '../types'
+
+export const profilesMap: Map = {
+  BARREL: {
+    label: 'Barrel',
+    image: '/img/svg/icon-profile-barrel.svg',
+    slug: 'barrel',
+  },
+  'CLASSIC COURT': {
+    label: 'Classic Court',
+    image: '/img/svg/icon-profile-classic-court.svg',
+    slug: 'classic-court',
+  },
+  Flat: {
+    label: 'Flat',
+    image: '/img/svg/icon-profile-flat.svg',
+    slug: 'flat',
+  },
+  'MODERN COURT': {
+    label: 'Modern Court',
+    image: '/img/svg/icon-profile-modern-court.svg',
+    slug: 'modern-court',
+  },
+  OTHER: {
+    label: 'Other',
+    image: '/img/svg/icon-profile-other.svg',
+    slug: 'other',
+  },
+  'D SHAPED': {
+    label: 'D-Shaped',
+    image: '/img/svg/icon-profile-d-shaped.svg',
+    slug: 'd-shaped',
+  },
+}
