6801e26a9f35a25f66a92547fa2db510b1b344da
Neil Mills <mail@neil-mills.co.uk>
2024-03-08 14:35:38 +0000

    Category banner component

--- app/components/CategoryBanner.tsx
+++ app/components/CategoryBanner.tsx
@@ -1,17 +1,17 @@
-import React from 'react'
+import { Mapping } from '@/app/types'
 
-export const CategoryBanner = () => {
+export const CategoryBanner = ({ category }: { category: Mapping }) => {
   return (
     <div className="inner-banner bg-gradient-grey mb-225rem">
       <div className="row g-0 align-items-center justify-content-center">
         <div className="col-7 col-sm-5">
-          <h3 className="ms-3 ms-sm-0">Solitaires</h3>
+          <h3 className="ms-3 ms-sm-0">{category.label}</h3>
         </div>
         <div className="col-5 col-sm-4">
           <img
             className="img-fluid w-100"
-            src="/img/01_solitaires.png"
-            alt="Product Style"
+            src={category.image}
+            alt={category.label}
           />
         </div>
       </div>
