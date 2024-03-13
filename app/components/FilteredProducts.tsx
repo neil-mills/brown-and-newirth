e7b28d39e9d2b38fcba3ce2da86ac148eee4a91d
Neil Mills <mail@neil-mills.co.uk>
2024-03-13 10:23:52 +0000

    Fix ts compile errors

--- app/components/FilteredProducts.tsx
+++ app/components/FilteredProducts.tsx
@@ -1,13 +1,14 @@
 'use client'
 import { ProductGrid, TitleBar } from '@/app/components'
 import { useProducts } from '@/app/hooks'
+import { ProductAttributes } from '../types'
 
 export const FilteredProducts = ({
   category,
   filters,
 }: {
   category: string
-  filters: Record<string, string> | null
+  filters: Record<ProductAttributes, string> | null
 }) => {
   const { products, isLoading, error } = useProducts(category, filters)
   if (isLoading || error) return null
