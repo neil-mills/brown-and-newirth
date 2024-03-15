9eabcd780f0fb9e85f06602375dcac0b4a6d98d9
Neil Mills <mail@neil-mills.co.uk>
2024-03-15 09:18:55 +0000

    Include all variations in product when filtering variation filter options

--- app/hooks/useVariationFilterOptions.ts
+++ app/hooks/useVariationFilterOptions.ts
@@ -1,17 +1,18 @@
-import { Mapping, Variation, VariationGauge, VariationWidth } from '@/app/types'
+import { Map, Mapping, Variation, Filters } from '@/app/types'
 import { useStore } from '@/app/hooks'
 import { getUniqueArrayValues } from '@/app/utils'
 import { widthMap, gaugeMap } from '@/app/maps'
 
 interface Props {
   filter: 'pa_gauge' | 'pa_width'
-  filters?: Record<string, string>
+  filters?: Filters
 }
 
-type Filters<T> = { [K in keyof T]: string }
+type FilterAttributes = 'pa_gauge' | 'pa_width'
 
-type VariationFilters = Filters<VariationGauge> | Filters<VariationWidth>
-const map = {
+type FilterMap = { [K in FilterAttributes]: Map }
+
+const map: FilterMap = {
   pa_gauge: gaugeMap,
   pa_width: widthMap,
 }
@@ -22,20 +23,20 @@ export const useVariationFilterOptions = ({
   let options: Mapping[] = []
   let filteredVariations: Variation[] = []
   const filterMap = map[filter]
-  const { variations } = useStore((store) => store.selectedSku)
-  if (variations) {
-    filteredVariations = variations
+  const { product } = useStore((store) => store.selectedSku)
+  if (product?.variations) {
+    filteredVariations = product.variations
     if (filters) {
       Object.entries(filters).forEach(([filter, value]) => {
         filteredVariations = filteredVariations.filter(
           (variation) =>
-            variation?.attributes?.[filter] &&
-            variation.attributes[filter] === value
+            variation?.attributes?.[filter as FilterAttributes] &&
+            variation.attributes[filter as FilterAttributes] === value
         )
       })
     }
-    options = getUniqueArrayValues(
-      variations
+    options = getUniqueArrayValues<string[]>(
+      product.variations
         .filter((variation) => variation?.attributes?.[filter])
         .map((variation) => variation.attributes[filter]!)
     ).map((value) => filterMap[value])
