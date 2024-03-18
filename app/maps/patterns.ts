534bb4cf9686f859c03a2a1876c3558da9c95f24
Neil Mills <mail@neil-mills.co.uk>
2024-03-18 15:38:43 +0000

    Add profile, pattern and ceramic colour icons

--- app/maps/patterns.ts
+++ app/maps/patterns.ts
@@ -5,32 +5,32 @@ type PatternMap = { [K in ProductPatterns]: Mapping }
 export const patternMap: PatternMap = {
   CELTIC: {
     label: 'Celtic',
-    image: '/img/svg/icon-shape-cutaway.svg',
+    image: '/img/svg/icon-pattern-celtic.svg',
     slug: 'celtic',
   },
   CERAMIC: {
     label: 'Ceramic',
-    image: '/img/svg/icon-shape-pinch.svg',
+    image: '/img/svg/icon-pattern-ceramic.svg',
     slug: 'ceramic',
   },
   CONTEMPORARY: {
     label: 'Contemporary',
-    image: '/img/svg/icon-shape-twist.svg',
+    image: '/img/svg/icon-pattern-contemporary.svg',
     slug: 'contemporary',
   },
   SPARKLE: {
     label: 'Sparkle',
-    image: '/img/svg/icon-shape-wishbone.svg',
+    image: '/img/svg/icon-pattern-sparkle.svg',
     slug: 'sparkle',
   },
   TRINITY: {
     label: 'Trinity',
-    image: '/img/svg/icon-shape-wishbone.svg',
+    image: '/img/svg/icon-pattern-trinity.svg',
     slug: 'trinity',
   },
   'MIXED METAL': {
     label: 'Mixed Metal',
-    image: '/img/svg/icon-shape-wishbone.svg',
+    image: '/img/svg/icon-pattern-mixed-metal.svg',
     slug: 'mixed-metal',
   },
 }
